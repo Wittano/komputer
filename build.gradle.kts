1a9b930d2c7937e9dfe4323e00081dd97a3ecd56
Wittano <radoslaw.ratyna@gmail.com>
2023-08-29 01:15:24 +0200

    chore(init): added necessary dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.9.10"
+    kotlin("kapt") version "1.9.0"
+
     application
 }
 
@@ -12,8 +14,23 @@ repositories {
     mavenCentral()
 }
 
+val picocliVersion = "4.7.4"
+
 dependencies {
+    implementation("com.discord4j:discord4j-core:3.2.5")
+    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("info.picocli:picocli:$picocliVersion")
+
+    kapt("info.picocli:picocli-codegen:$picocliVersion")
+
     testImplementation(kotlin("test"))
+}
+
+kapt {
+    arguments {
+        arg("project", "${project.group}/${project.name}")
+    }
 }
 
 tasks.test {
