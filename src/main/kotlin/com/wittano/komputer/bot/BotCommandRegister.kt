e466e0b3fbb0e9a18248e5fae0e8a2e2182b986b
Wittano <radoslaw.ratyna@gmail.com>
2023-08-30 00:07:18 +0200

    refactor(commands): added dependency injection for BotCommandRegister

--- src/main/kotlin/com/wittano/komputer/bot/BotCommandRegister.kt
+++ src/main/kotlin/com/wittano/komputer/bot/BotCommandRegister.kt
@@ -1,18 +1,17 @@
 package com.wittano.komputer.bot
 
-import com.wittano.komputer.command.AddJokeCommand
-import com.wittano.komputer.command.JokeCommand
-import com.wittano.komputer.command.WelcomeCommand
+import com.google.inject.Inject
+import com.wittano.komputer.command.SlashCommand
 import com.wittano.komputer.config.ConfigLoader
 import discord4j.rest.RestClient
 import org.slf4j.LoggerFactory
 
-class BotCommandRegister(private val client: RestClient) {
-
+class BotCommandRegister @Inject constructor(
+    private val commands: Set<SlashCommand>,
+) {
     private val logger = LoggerFactory.getLogger(BotCommandRegister::class.java)
-    private val commands = arrayListOf(AddJokeCommand(), JokeCommand(), WelcomeCommand())
 
-    fun singIn() {
+    fun singIn(client: RestClient) {
         val config = ConfigLoader.load()
 
         commands.forEach {
