ad311d149d07cc4dc02d18f83f0a00fdb2ccf759
Wittano <radoslaw.ratyna@gmail.com>
2023-08-29 23:56:16 +0200

    feat(guice): added guice dependency injector

--- src/main/kotlin/com/wittano/komputer/Main.kt
+++ src/main/kotlin/com/wittano/komputer/Main.kt
@@ -1,6 +1,8 @@
 package com.wittano.komputer
 
+import com.google.inject.Guice
 import com.wittano.komputer.bot.KomputerBot
+import com.wittano.komputer.config.guice.GuiceCommandsModule
 import org.slf4j.LoggerFactory
 import picocli.CommandLine
 import kotlin.system.exitProcess
@@ -9,7 +11,9 @@ fun main(args: Array<String>) {
     val logger = LoggerFactory.getLogger("MAIN")
 
     try {
-        CommandLine(KomputerBot()).execute(*args)
+        val injector = Guice.createInjector(GuiceCommandsModule())
+
+        CommandLine(KomputerBot(injector)).execute(*args)
     } catch (ex: Exception) {
         logger.error("Unhandled exception: ${ex.message}")
         exitProcess(-1)
