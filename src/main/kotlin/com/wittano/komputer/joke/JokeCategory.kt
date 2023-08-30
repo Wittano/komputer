9be67e9b4d2b7e066877135181558f1e8ec49e32
Wittano <radoslaw.ratyna@gmail.com>
2023-08-30 02:15:22 +0200

    fix(category): changed PROGRAMMING polish translate

--- src/main/kotlin/com/wittano/komputer/joke/JokeCategory.kt
+++ src/main/kotlin/com/wittano/komputer/joke/JokeCategory.kt
@@ -1,7 +1,7 @@
 package com.wittano.komputer.joke
 
 enum class JokeCategory(val category: String, val polishTranslate: String) {
-    PROGRAMMING("Programming", "Straszne"),
+    PROGRAMMING("Programming", "Programowanie"),
     ANY("Any", "Dowolne"),
     MISC("Misc", "Straszne"),
     DARK("Dark", "Czarny humor"),
