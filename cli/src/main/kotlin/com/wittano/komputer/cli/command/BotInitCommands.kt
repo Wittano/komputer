5fea6e431bef812746f9b0af6376e95228c8b2c2
Wittano <radoslaw.ratyna@gmail.com>
2023-09-05 22:56:31 +0200

    feat(cli/inti): added init command - command to register slash command into guild

--- cli/src/main/kotlin/com/wittano/komputer/cli/command/BotInitCommands.kt
+++ cli/src/main/kotlin/com/wittano/komputer/cli/command/BotInitCommands.kt
@@ -1,13 +1,34 @@
 package com.wittano.komputer.cli.command
 
+import com.wittano.komputer.cli.discord.command.RegisteredCommandsUtils
+import com.wittano.komputer.core.bot.discordClient
+import com.wittano.komputer.core.config.config
+import org.slf4j.LoggerFactory
 import picocli.CommandLine.Command
+import reactor.core.publisher.Flux
 
 @Command(
     name = "init",
     description = ["Register slash commands on specified server or global"]
 )
 class BotInitCommands : Runnable {
+    private val log = LoggerFactory.getLogger(this::class.qualifiedName)
+
     override fun run() {
-        TODO("Not yet implemented")
+        val commands = RegisteredCommandsUtils.getCommandsFromJsonFiles()
+
+        Flux.fromIterable(commands)
+            .flatMap {
+                discordClient.restClient.applicationService.createGuildApplicationCommand(
+                    config.applicationId,
+                    config.guildId,
+                    it
+                )
+            }.doOnComplete {
+                log.info("Komputer's commands was created successfully")
+
+            }.collectList().block()
+
+        discordClient.logout().block()
     }
 }
