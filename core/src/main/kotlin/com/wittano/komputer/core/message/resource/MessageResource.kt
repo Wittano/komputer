5a7fe3c9cbde79ad7bb1cb37182187758cbafb04
Wittano <radoslaw.ratyna@gmail.com>
2023-09-09 10:43:18 +0200

    refactor(translation): added method translation into MessageResource.kt

--- core/src/main/kotlin/com/wittano/komputer/core/message/resource/MessageResource.kt
+++ core/src/main/kotlin/com/wittano/komputer/core/message/resource/MessageResource.kt
@@ -2,8 +2,16 @@ package com.wittano.komputer.core.message.resource
 
 import java.util.*
 
-internal fun getErrorMessage(code: ErrorMessage, locale: Locale = Locale.ENGLISH): String {
+private val POLISH_LOCALE = Locale("pl")
+
+internal fun getErrorMessage(code: ErrorMessage, locale: Locale = POLISH_LOCALE): String {
     val resourceBundle = ResourceBundle.getBundle("i18n.error-message", locale)
 
     return resourceBundle.getString(code.code)
+}
+
+internal fun getButtonLabel(name: ButtonLabel, locale: Locale = POLISH_LOCALE): String {
+    val resource = ResourceBundle.getBundle("i18n.reaction-button", locale)
+
+    return resource.getString(name.code)
 }
