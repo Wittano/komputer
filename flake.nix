22769a18356ab0eb459061bb5b0880d79b45684b
Wittano <radoslaw.ratyna@gmail.com>
2023-10-29 21:50:39 +0100

    feat(nix): added nix derivation

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "komputer - discord bot";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+
+      lib = nixpkgs.lib.extend (self: super: {
+        my = import ./lib { inherit pkgs; lib = self; };
+      });
+    in rec {
+      inherit lib;
+
+      packages.x86_64-linux.komputer = pkgs.callPackage ./default.nix {};
+
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.komputer;
+
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          go
+          ffmpeg
+          rnix-lsp
+          nixfmt
+          nixpkgs-fmt
+        ];
+      };
+    };
+}
