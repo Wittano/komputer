52fbf7107441c0bccc490a56952657e5b3c9d638
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2024-04-13 18:12:49 +0200

    feat(api): added health checker

--- web/server.go
+++ web/server.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/wittano/komputer/web/internal/handler"
@@ -16,13 +17,16 @@ func NewWebConsoleServer(configPath string) (*echo.Echo, error) {
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.BodyLimit(fmt.Sprintf("%dM", settings.Config.Upload.MaxFileSize)))
 
-	e.POST("/api/v1/audio", handler.UploadNewAudio)
 	e.GET("/api/v1/audio/:id", handler.GetAudio)
+	e.POST("/api/v1/audio", handler.UploadNewAudio)
 	e.DELETE("/api/v1/audio/:id", handler.RemoveAudio)
 
 	e.GET("/api/v1/setting", handler.GetSettings)
 	e.PUT("/api/v1/setting", handler.UpdateSettings)
+
+	e.GET("/api/v1/health", handler.HealthChecker)
 
 	return e, nil
 }
